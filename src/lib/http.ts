829447128e6a74f206f2faf1f9c95f88edffbdb0
ano373 <andrewamin37@gmail.com>
2025-10-08 13:59:06 +0300

    fix missing https in url

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 export const http = axios.create({
-  baseURL: "http://byway-fs132.runasp.net/api",
+  baseURL: "https://byway-fs132.runasp.net/api",
   timeout: 5000,
 });
 
